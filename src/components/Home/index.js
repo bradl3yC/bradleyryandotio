d93e705cc26aba553b717d50236b3b39f31fba32
Brad Conley <brad.conl3y@gmail.com>
2017-09-21 10:38:35 -0400

    Add additional typing effect

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Typing from 'react-typing-animation'
 import TypingArea from './components/TypingArea'
+import Typed from './components/Typed'
 
 class Home extends Component {
   constructor(props) {
@@ -17,14 +18,19 @@ class Home extends Component {
   render() {
     return (
       <TypingArea hidden={this.state.hidden}>
-        <Typing speed={40} onFinishedTyping={this.disableVisibility}>
-          <p>&#60;h1&#62;BradleyRyan.io&#60;&#47;h1&#62;</p>
+        <Typing speed={40}>
+          <Typed>&#60;h1&#62;BradleyRyan.io&#60;&#47;h1&#62;</Typed>
           <Typing.Delay ms={1000}/>
-          <p>git add index.js</p>
+          <Typed>git add index.js</Typed>
           <Typing.Delay ms={1000}/>
-          <p>git commit -m "Build all the things"</p>
-          <Typing.Delay ms={1000}/>
-          <p>git push</p>
+          <Typed>git commit -m "Build all the things"</Typed>
+        </Typing>
+        <Typing>
+
+        </Typing>
+        <Typing onFinishedTyping={this.disableVisibility}>
+          <Typing.Delay ms={5000}/>
+          <Typed>git push</Typed>
           <Typing.Delay ms={800}/>
         </Typing>
       </TypingArea>
