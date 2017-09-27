b48ef9ecb598713c236f8f522de303866b7c0ad9
Brad Conley <brad.conl3y@gmail.com>
2017-09-27 14:00:59 -0400

    Remove uncessary images

--- src/components/TechStack/index.js
+++ src/components/TechStack/index.js
@@ -9,15 +9,13 @@ import Colorize from '../global_components/Colorize';
 import Main from './components/Main';
 // Images
 import javascriptImage from '../../images/javascript-plain.svg';
-import reactImage from "../../images/react-original.svg"
-import nodeImage from "../../images/nodejs-plain.svg"
-import railsImage from "../../images/rails-plain.svg"
-import rubyImage from "../../images/ruby-plain.svg"
-import html5Image from "../../images/html5-plain.svg"
-import css3Image from "../../images/css3-plain.svg"
-import atomImage from "../../images/atom-original.svg"
-import herokuImage from "../../images/heroku-plain.svg"
-import yarnImage from "../../images/yarn-original.svg"
+import reactImage from "../../images/react-original.svg";
+import nodeImage from "../../images/nodejs-plain.svg";
+import railsImage from "../../images/rails-plain.svg";
+import rubyImage from "../../images/ruby-plain.svg";
+import html5Image from "../../images/html5-plain.svg";
+import css3Image from "../../images/css3-plain.svg";
+import herokuImage from "../../images/heroku-plain.svg";
 
 const TechStack = () => (
   <Main>
@@ -27,14 +25,12 @@ const TechStack = () => (
       <Image src={reactImage}/>
       <Image src={nodeImage}/>
       <Image src={railsImage}/>
-      <Image src={rubyImage}/>
     </ImageContainer>
     <ImageContainer>
+      <Image src={rubyImage}/>
       <Image src={html5Image}/>
       <Image src={css3Image}/>
-      <Image src={atomImage}/>
       <Image src={herokuImage}/>
-      <Image src={yarnImage}/>
     </ImageContainer>
   </Main>
 );
