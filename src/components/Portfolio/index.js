5e2c47639b11da068f633fbb5a72e800bbbcbadc
Brad Conley <brad.conl3y@gmail.com>
2017-10-19 10:53:38 -0400

    Add Apptly

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -11,12 +11,23 @@ import PortfolioText from './components/PortfolioText';
 import PortfolioLink from './components/PortfolioLink';
 import hangmanImage from '../../images/8bit-hangman.png';
 import portfolioImage from '../../images/bradley-ryan.png';
+import apptlyImage from '../../images/apptly.png';
 
 
 
 const Portfolio = () => (
   <Main>
     <PortfolioContainer>
+      <Heading>&#60;<Colorize color={"#fec242"}> Apptly </Colorize>/&#62;</Heading>
+      <PortfolioLink href="https://apptly-app.herokuapp.com" >
+        <PortfolioImage src={apptlyImage}/>
+      </PortfolioLink>
+      <PortfolioLink href="https://github.com/bradl3yC/appointments">https://github.com/bradl3yC/appointments</PortfolioLink>
+      <PortfolioText>
+        Apptly was written with React and it consumes an API written in Ruby on Rails 5.1.
+        Apptly utilizes the Google geocode API to find appointment address and place them on a responsive map.
+      </PortfolioText>
+      <PortfolioLink href="https://github.com/bradl3yC/api-appointments">https://github.com/bradl3yC/api-appointments</PortfolioLink>
       <Heading>&#60;<Colorize color={"#fec242"}> 8Bit Hangman </Colorize>/&#62;</Heading>
       <PortfolioLink href="https://hangman-8bit.herokuapp.com/" >
         <PortfolioImage src={hangmanImage}/>
